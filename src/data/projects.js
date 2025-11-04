ee3042c97e25ba48f13d619d9253511ebaa55c3b
vaishnavi gaikwad <vaishgaikwad472@gmail.com>
2025-11-04 21:29:54 +0530

    add github links

--- src/data/projects.js
+++ src/data/projects.js
@@ -16,7 +16,7 @@ summary: 'Stay updated with the latest news from various categories.',
 description:
 'A modern news application that fetches the latest news from a free public API. Users can browse news by categories, search for specific topics, and view detailed articles. Built with React, Fetch API, and TailwindCSS for a responsive and smooth user experience.',
 tech: ['React', 'Fetch', 'TailwindCSS', 'REST API'],
-repo: '[https://github.com/vaishgaikwad04/News-App-.git](https://github.com/vaishgaikwad04/News-App-.git)',
+repo: 'https://github.com/vaishgaikwad04/News-App-.git',
 live: '[https://news-app-ba8j.onrender.com](https://news-app-ba8j.onrender.com)',
 },
 {
@@ -41,7 +41,7 @@ summary: 'A full-stack Zara-style fashion e-commerce website built with the MERN
 description:
 'A Zara-inspired fashion e-commerce platform built using the MERN stack. It includes user authentication, protected routes, and role-based access. Users can explore collections, add products to the cart, like or comment on products, and make secure payments via Stripe. The admin dashboard allows complete control over product, order, and user management through MongoDB. Styled with TailwindCSS for a clean, minimal, and fully responsive look inspired by Zara’s design aesthetic.',
 tech: ['MongoDB', 'Express', 'React', 'Node.js', 'Stripe', 'TailwindCSS', 'JWT', 'Protected Routes'],
-repo: '[https://github.com/vaishgaikwad04/Tira-ecommerce.git](https://github.com/vaishgaikwad04/Tira-ecommerce.git)',
+repo: 'https://github.com/vaishgaikwad04/Tira-ecommerce.git',
 live: '[https://tira-ecommerce.onrender.com](https://tira-ecommerce.onrender.com)',
 },
 {
@@ -65,7 +65,7 @@ summary: 'A complete MERN-based platform combining an e-commerce store with inte
 description:
 'This is a modern MERN stack web application that blends e-commerce functionality with a full-featured blog system. Users can read and comment on blogs, explore product collections. Includes CRUD operations for blogs and products, and a clean TailwindCSS UI. Built for seamless integration between content and commerce.',
 tech: ['MongoDB', 'Express', 'React', 'Node.js', 'TailwindCSS', 'REST API'],
-repo: '[https://github.com/vaishgaikwad04/Ecommerce-website.git](https://github.com/vaishgaikwad04/Ecommerce-website.git)',
+repo: 'https://github.com/vaishgaikwad04/Ecommerce-website.git',
 live: '[https://tira-ecommerce.onrender.com](https://tira-ecommerce.onrender.com)',
 },
 
